1e73eaaf1f45a589b7dc55c7b831bf8d468011a9
ian firmino <ian.firmino@hotmail.com>
2023-05-01 16:23:30 -0300

    adjust

---
+++ 03-greeting-service/src/test/java/br/com/ian/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
